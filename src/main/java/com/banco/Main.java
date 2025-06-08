0269cf1c9279518f2b7a33e5bddf8cfb7998b3e1
lucas-lima-duarte <lucas_16_lima@hotmail.com>
2025-06-08 11:41:12 -0300

    feat: add initial menu

--- src/main/java/com/banco/Main.java
+++ src/main/java/com/banco/Main.java
@@ -3,6 +3,9 @@ package com.banco;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bem-vindo ao Banco!");
+        System.out.println("Informe a opção desejada");
+        System.out.println("1. Criaçao de conta");
+        System.out.println("2. Sair");
 
         Conta conta = new Conta(1, "Teste", 10, 5);
 
