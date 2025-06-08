c847576b1e9213501845dc6c058b53771c8155d3
Bianca <biancalenineb.schons@hotmail.com>
2025-06-08 11:38:21 -0300

    chore: instancia de conta na classe Main

--- src/main/java/com/banco/Conta.java
+++ src/main/java/com/banco/Conta.java
@@ -1,10 +1,10 @@
 package com.banco;
 
 public class Conta {
-    private int numero;
-    private String nomeCliente;
-    private int saldo;
-    private int limiteCredito;
+    public int numero;
+    public String nomeCliente;
+    public int saldo;
+    public int limiteCredito;
 
     public Conta(int numero, String nomeCliente, int saldo, int limiteCredito) {
         this.numero = numero;
--- src/main/java/com/banco/Main.java
+++ src/main/java/com/banco/Main.java
@@ -3,5 +3,9 @@ package com.banco;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bem-vindo ao Banco!");
+
+        Conta conta = new Conta(1, "Teste", 10, 5);
+
+        System.out.println(conta.nomeCliente);
     }
 } 
