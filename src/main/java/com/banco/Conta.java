27eaed5cb313a6c2123be28ae9b828d4f326d6b8
lucas-lima-duarte <lucas_16_lima@hotmail.com>
2025-06-08 11:26:07 -0300

    feat: conta constructor implemented

--- src/main/java/com/banco/Conta.java
+++ src/main/java/com/banco/Conta.java
@@ -1,8 +1,17 @@
 package com.banco;
 
 public class Conta {
-    private int numeroConta;
+    private int numero;
     private String nomeCliente;
     private int saldo;
     private int limiteCredito;
+
+    public Conta(int numero, String nomeCliente, int saldo, int limiteCredito) {
+        this.numero = numero;
+        this.nomeCliente = nomeCliente;
+        this.saldo = saldo;
+        this.limiteCredito = limiteCredito;
+    }
+
 }
+
